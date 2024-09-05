dad4786c596a0825e1bbe41a5073b1be340c8a84
ckarrer <47241179+ckarrer@users.noreply.github.com>
2024-09-05 17:25:47 +0200

    Fix compilation by adding gaussian_3d_coeff

--- geometry/gaussian_io.py
+++ geometry/gaussian_io.py
@@ -264,6 +264,27 @@ class GaussianIO:
 
         return occ
 
+    def gaussian_3d_coeff(xyzs, covs):
+        # xyzs: [N, 3]
+        # covs: [N, 6]
+        x, y, z = xyzs[:, 0], xyzs[:, 1], xyzs[:, 2]
+        a, b, c, d, e, f = covs[:, 0], covs[:, 1], covs[:, 2], covs[:, 3], covs[:, 4], covs[:, 5]
+    
+        # eps must be small enough !!!
+        inv_det = 1 / (a * d * f + 2 * e * c * b - e**2 * a - c**2 * d - b**2 * f + 1e-24)
+        inv_a = (d * f - e**2) * inv_det
+        inv_b = (e * c - b * f) * inv_det
+        inv_c = (e * b - c * d) * inv_det
+        inv_d = (a * f - c**2) * inv_det
+        inv_e = (b * c - e * a) * inv_det
+        inv_f = (a * d - b**2) * inv_det
+    
+        power = -0.5 * (x**2 * inv_a + y**2 * inv_d + z**2 * inv_f) - x * y * inv_b - x * z * inv_c - y * z * inv_e
+    
+        power[power > 0] = -1e10 # abnormal values... make weights 0
+            
+        return torch.exp(power)
+    
     def extract_mesh(self, density_thresh=0.8, resolution=128, decimate_target=1e5):
         occ = self.extract_fields(resolution).detach().cpu().numpy()
 
